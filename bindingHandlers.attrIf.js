71f30fdac4a05f3fcf7c5dfed55b3b14b7df9cc5
Timothy Wood <tim@imaginesimplicity.com>
2014-04-10 14:43:31 -0400

    Create bindingHandlers.attrIf.js

---
+++ bindingHandlers.attrIf.js
@@ -0,0 +1,20 @@
+/**
+ * check the value of a specific observable boolean before add or not the attributes
+ *
+ */
+
+ko.bindingHandlers.attrIf = {
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var h = ko.utils.unwrapObservable(valueAccessor());
+        var ifShow = ko.utils.unwrapObservable(h._if);
+        if (ifShow) {
+            ko.bindingHandlers.attr.update(element, valueAccessor, allBindingsAccessor);
+        } else {
+            for (var k in h) {
+                if (h.hasOwnProperty(k) && k.indexOf("_") !== 0) {
+                    $(element).removeAttr(k);
+                }
+            }
+        }
+    }
+};

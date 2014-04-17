4c17e92097553a01f0e643cbcaa9611130dbde22
Timothy Wood <tim@imaginesimplicity.com>
2014-04-17 09:34:15 -0400

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    meta : {
+      banner : '/*!\n' +
+      ' * <%= pkg.main %> v<%= pkg.version %>\n' +
+      ' * <%= pkg.homepage %>\n' +
+      ' *\n' +
+      ' * Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author %>\n' +
+      ' * Released under the <%= pkg.license %> License.\n' +
+      ' */\n\n'
+    },
+
+    concat: {
+      options: {
+        banner: '<%= meta.banner %>'
+      },
+      dist: {
+        src: [
+          'lib/bindingHandlers.attrIf.js',
+          'lib/observableArray.distinct.js',
+          'lib/observableArray.pushAll.js',
+          'lib/observableArray.pushAt.js',
+          'lib/observableArray.totalVisible.js',
+          'lib/bindingHandlers.select2.js'
+        ],
+        dest: '<%= pkg.main %>'
+      }
+    },
+
+    watch : {
+      files : '<%= concat.dist.src %>',
+      tasks : 'default'
+    },
+
+    jshint : {
+      all : ['Gruntfile.js']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['test', 'concat']);
+};
